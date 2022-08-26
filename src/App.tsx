c078c127418a1af955c6f932a780422e002b342d
Jayce <thainguyenhoangphatit@gmail.com>
2022-08-26 15:36:07 +0700

    Eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import "styles/tailwind.scss";
 import "./App.scss";
 
-const App: React.FC = () => {
-  return <div className="App">{`Let's go`}</div>;
-};
+const App: React.FC = () => <div className="App">{`Let's go`}</div>;
 
 export default App;
