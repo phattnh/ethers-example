e0bb47c045fe6aee8b9c1d94296e8f1ae9545101
Jayce <thainguyenhoangphatit@gmail.com>
2022-08-26 16:28:39 +0700

    Re-deploy message

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "styles/tailwind.scss";
 import "./App.scss";
 
 const App: React.FC = () => {
-  return <div className="App">{`Let's go`}</div>;
+  return <div className="App">{`Let's go! We will build ethers app`}</div>;
 };
 
 export default App;
